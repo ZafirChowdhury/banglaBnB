84d99298c8d954e59e2be33e9d828076b5ade8d7
zafir <zafirchowdhury69@gmail.com>
2025-11-19 16:51:58 +0600

    Add register, login, logout code

--- app.py
+++ app.py
@@ -15,7 +15,15 @@ app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(days=7)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return "TODO : Index"
+    if request.method == "GET":
+        # If user is not logedin redirrect to login
+        if not session.get("user_id", None):
+            return redirect(url_for("login"))
+        
+        return f"You are logged in as {session}"
+        
+    if request.method == "POST":
+        return "TODO : Index : POST : Search"
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -24,7 +32,29 @@ def register():
         return render_template("/register.html")
 
     if request.method == "POST":
-        return "TODO : Register Post"
+        user_name = request.form.get("user_name", None)
+        email = request.form.get("email", None)
+        password = request.form.get("password", None)
+        password_again = request.form.get("password_again", None)
+
+        if not user_name or not email or not password or not password_again:
+            return redirect(url_for("apology", em="Please fill all the requred fields."))
+        
+        if password != password_again:
+            return redirect(url_for("apology", em="confirmation password dose not match."))
+
+        # Check Username or email is already exist
+        user_list = database.get("SELECT * FROM users WHERE user_name = %s or email = %s", 
+                                 (user_name, email))
+        
+        if len(user_list) >= 1:
+            return redirect(url_for("apology", em="Username allready taken or email allready in use."))
+
+        # Hasing the password and saving the user to the database
+        database.save("INSERT INTO users (user_name, email, password_hash) VALUES (%s, %s, %s)", 
+                      (user_name, email, str(generate_password_hash(password))))
+
+        return redirect("login")
     
 
 @app.route("/login", methods=["GET", "POST"])
@@ -35,10 +65,34 @@ def login():
         return render_template("/login.html")
 
     if request.method == "POST":
-        return "TODO : Login POST"
+        user_name = request.form.get("user_name", None)
+        password = request.form.get("password", None)
+
+        if not user_name or not password:
+            return redirect(url_for("apology", em="Please fill all the requred fields"))
+
+        # Check if the user exists or not
+        user_list =  database.get("SELECT * FROM users WHERE user_name = %s or email = %s", (user_name, user_name))
+        if len(user_list) == 0:
+            return redirect(url_for("apology", em="Username or Email dose not exist"))
+        
+        # Checking Password
+        user = user_list[0]
+        if check_password_hash(user.get("password_hash"), password):
+            session["user_id"] = user.get("user_id")
+            session["is_admin"] = user.get("is_admin")
+            return redirect(url_for("index"))
+        else:
+            return redirect(url_for("apology", em="Wrong Password"))
     
 
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect("login")
+
+
+# em = Error Massage
+@app.route("/apology")
+def apology():
+    return render_template("apology.html", em=request.args.get("em", "No Error"))
